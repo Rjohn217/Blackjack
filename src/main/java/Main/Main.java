093b55af37c3a7f9963f643a355039f2aca6b3af
Austin <austinjohnson1203@gmail.com>
2023-03-01 04:10:37 -0500

    Added welcome message to main.

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,6 +2,7 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("");
+        System.out.println("****Welcome to the Blackjack Table!****");
     }
 }
