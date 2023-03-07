39d7bffb7a8bc3b2ecae10188506113363bc0003
Austin <austinjohnson1203@gmail.com>
2023-03-07 14:18:08 -0500

    shuffle() added

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,8 +1,17 @@
 package Main;
 
+import Model.Deck;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("");
         System.out.println("****Welcome to the Blackjack Table!****");
+
+        Deck playingDeck = new Deck();
+        playingDeck.createFullDeck();
+        playingDeck.shuffle();
+
+        System.out.println(playingDeck);
+
     }
 }
--- src/main/java/Model/Deck.java
+++ src/main/java/Model/Deck.java
@@ -2,13 +2,14 @@ package Model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Deck {
 
-    List<Card> cards = new ArrayList<>();
+    private ArrayList<Card> cards;
 
-    public Deck(List<Card> cards) {
-        this.cards = cards;
+    public Deck() {
+        this.cards = new ArrayList<Card>();
     }
 
     public void createFullDeck() {
@@ -20,7 +21,16 @@ public class Deck {
     }
 
     public void shuffle()  {
-
+        ArrayList<Card> tempDeck = new ArrayList<Card>();
+        Random random = new Random();
+        int randomCardIndex = 0;
+        int originalSize = this.cards.size();
+        for(int i = 0; i < originalSize; i++) {
+            randomCardIndex = random.nextInt((this.cards.size() - 1 - 0) + 1) + 0;
+            tempDeck.add(this.cards.get(randomCardIndex));
+            this.cards.remove(randomCardIndex);
+        }
+        this.cards = tempDeck;
     }
 
     public void removeCard() {
@@ -48,7 +58,13 @@ public class Deck {
     }
 
     public String toString() {
-        return null;
+        String cardListOutput = "";
+        int i = 0;
+        for(Card card: this.cards) {
+            cardListOutput += "\n" + i + " " + card.toString();
+            i++;
+        }
+        return cardListOutput;
     }
 
 }
