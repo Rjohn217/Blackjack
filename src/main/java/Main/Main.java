840690eb877ddc88995d4ef411ccde04305546f1
Austin <austinjohnson1203@gmail.com>
2023-03-07 15:30:54 -0500

    Game functions, but logic for handing money and adding drawn card to player deck is wrong.

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,14 +2,99 @@ package Main;
 
 import Model.Deck;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
-        System.out.println("****Welcome to the Blackjack Table!****");
+        System.out.println("\n**** Welcome to the Blackjack Table! ****\n****\t  The game pays 2:1\t\t\t****\n");
 
         Deck playingDeck = new Deck();
         playingDeck.createFullDeck();
         playingDeck.shuffle();
+        Deck playerDeck = new Deck();
+        Deck dealerDeck = new Deck();
+        double playerMoney = 100.00;
+        Scanner scanner = new Scanner(System.in);
+
+        while (playerMoney > 0) {
+            System.out.println("You have $" + playerMoney + ", how much would you like to bet?");
+            double playerBet = scanner.nextDouble();
+
+            if(playerBet > playerMoney) {
+                System.out.println("You can't bet more money than you have, you can go now.");
+                break;
+            }
+
+            boolean endRound = false;
+
+            playerDeck.drawCard(playingDeck);
+            playerDeck.drawCard(playingDeck);
+            dealerDeck.drawCard(playingDeck);
+            dealerDeck.drawCard(playingDeck);
+
+            while(true) {
+                System.out.println("Your hand: " + playerDeck.toString());
+                System.out.println("Dealer hand: " + dealerDeck.getCard(0).toString() + " and [Hidden Card]");
+                System.out.println("Would you like to Hit(1) or Stand(2)");
+                int response = scanner.nextInt();
+
+                if(response == 1) {
+                    playerDeck.drawCard(playingDeck);
+                    System.out.println("You draw a: " + playingDeck.getCard(playerDeck.deckSize() - 1).toString() + "\n");
+                    if(playerDeck.cardsValue() > 21) {
+                        System.out.println("You Busted! Deck Value: " + playerDeck.cardsValue());
+                        playerMoney = playerMoney - playerBet;
+                        endRound = true;
+                        break;
+                    }
+                } else if(response == 2) {
+                    break;
+                }
+            }
+
+            System.out.println("Dealer cards: " + dealerDeck.toString());
+            if(dealerDeck.cardsValue() > playerDeck.cardsValue() && endRound) {
+                System.out.println("The dealer wins, thanks for your generous donation");
+                playerMoney -= playerBet;
+                endRound = true;
+            }
+
+            while(dealerDeck.cardsValue() < 17 && endRound) {
+                dealerDeck.drawCard(playingDeck);
+                System.out.println("Dealer draws: " + dealerDeck.getCard(dealerDeck.deckSize() - 1).toString());
+            }
+
+            System.out.println("The value of the dealer's hand is: " + dealerDeck.cardsValue());
+
+            if(dealerDeck.cardsValue() > 21 && endRound){
+                System.out.println("Dealer has busted, you win this time..");
+                playerMoney += (playerBet * 1.5);
+                endRound = true;
+            }
+
+            if(playerDeck.cardsValue() == dealerDeck.cardsValue() && endRound){
+                System.out.println("Push");
+                endRound = true;
+            }
+
+            if(playerDeck.cardsValue() > dealerDeck.cardsValue() && endRound) {
+                System.out.println("You win this hand, but don't push your luck.");
+                playerMoney += (playerBet * 1.5);
+                endRound = true;
+            }
+            else if(endRound){
+                System.out.println("You lose the hand.");
+                playerMoney -= playerBet;
+                endRound = true;
+            }
+
+            playerDeck.moveAllToDeck(playingDeck);
+            dealerDeck.moveAllToDeck(playingDeck);
+            System.out.println("End of hand.");
+
+        }
+        System.out.println("You're out of money, thanks for your donations!");
+
 
         System.out.println(playingDeck);
 
--- src/main/java/Model/Deck.java
+++ src/main/java/Model/Deck.java
@@ -33,36 +33,72 @@ public class Deck {
         this.cards = tempDeck;
     }
 
-    public void removeCard() {
-
+    public void removeCard(int i) {
+        this.cards.remove(i);
     }
 
-    public void getCard() {
-
+    public Card getCard(int i) {
+        return this.cards.get(i);
     }
 
-    public void drawCard() {
-
+    public void addCard(Card card) {
+        this.cards.add(card);
     }
 
-    public void moveAllToDeck() {
+    public void drawCard(Deck deckFrom) {
+        this.cards.add(deckFrom.getCard(0));
+        deckFrom.removeCard(0);
+    }
 
+    public int cardsValue(){
+        int aces = 0;
+        int totalValue = 0;
+
+        for(Card card: this.cards) {
+            switch (card.getValue()){
+                case TWO: totalValue += 2; break;
+                case THREE: totalValue += 3; break;
+                case FOUR: totalValue += 4; break;
+                case FIVE: totalValue += 5; break;
+                case SIX: totalValue += 6; break;
+                case SEVEN: totalValue += 7; break;
+                case EIGHT: totalValue += 8; break;
+                case NINE: totalValue += 9; break;
+                case TEN:
+                case JACK:
+                case QUEEN:
+                case KING:
+                    totalValue += 10; break;
+                case ACE: totalValue += 1; break;
+            }
+        } for(int i = 0; i < aces; i++) {
+            if(totalValue > 10) {
+                totalValue += 1;
+            } else{
+                totalValue += 11;
+            }
+        } return totalValue;
+    }
+
+    public void moveAllToDeck(Deck moveTo) {
+        int thisDeckSize = this.cards.size();
+        for(int i = 0; i < thisDeckSize; i++) {
+            moveTo.addCard(this.getCard(i));
+        }
+        for(int i = 0; i < thisDeckSize; i++){
+            this.removeCard(0);
+        }
     }
 
     public int deckSize() {
-        return 52;
+        return this.cards.size();
     }
 
-    public int cardsValue() {
-        return 0;
-    }
 
     public String toString() {
         String cardListOutput = "";
-        int i = 0;
         for(Card card: this.cards) {
-            cardListOutput += "\n" + i + " " + card.toString();
-            i++;
+            cardListOutput += "\n" + card.toString();
         }
         return cardListOutput;
     }
